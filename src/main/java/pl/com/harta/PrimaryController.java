94e1bd7c24aa6e53f7bd8cde07b96102c23430a6
gallek <gallekpl@gmail.com>
2021-02-06 15:27:14 +0100

    some changes and it works now

--- src/main/java/pl/com/harta/PrimaryController.java
+++ src/main/java/pl/com/harta/PrimaryController.java
@@ -8,10 +8,7 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -25,6 +22,8 @@ public class PrimaryController{
     @FXML
     private ProgressBar pb;
     private String str;
+    @FXML
+    private Button searchButton;
 
 
 
@@ -36,49 +35,56 @@ public class PrimaryController{
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }*/
-    Task<Void> task = new Task<>() {
-       @Override
-       protected Void call() throws Exception {
-           StringBuilder sb = new StringBuilder();
-           String ratingString;
-           Document docRating;
-           Element elementRating;
 
-           int progress = 0;
-           int listSize = fs.list.size();
-           for (HashMap.Entry<String, String> entry : fs.list.entrySet()) {
-               if (isCancelled()) {
-                   updateMessage("Cancelled");
-                   break;
-               }
-
-               docRating = Jsoup.connect("https://www.imdb.com" + entry.getKey()).get();
-               elementRating = docRating.getElementsByClass("imdbRating").before("pro-logo-main-tittle").first();
-
-
-               if (elementRating == null) {
-                   ratingString = "N/A";
-               } else {
-                   ratingString = elementRating.text();
-                   ratingString = ratingString.split(" ")[0];
-               }
-               sb.append(ratingString).append(" ----> ").append(entry.getValue()).append("\n");
-               progress = progress + 1;
-               updateProgress(progress, listSize);
-           }
-           str = sb.toString();
-           ratingTextArea.setText(str);
-           updateProgress(0, listSize);
-           return null;
-       }
-   };
     @FXML
     private void setTextArea() throws IOException {
+        searchButton.setDisable(true);
+        Task<Void> task = new Task<>() {
+            @Override
+            protected Void call() throws Exception {
+                StringBuilder sb = new StringBuilder();
+                String ratingString;
+                Document docRating;
+                Element elementRating;
+
+                int progress = 0;
+                int listSize = fs.list.size();
+
+                for (HashMap.Entry<String, String> entry : fs.list.entrySet()) {
+                    if (isCancelled()) {
+                        updateMessage("Cancelled");
+                        break;
+                    }
+
+                    docRating = Jsoup.connect("https://www.imdb.com" + entry.getKey()).get();
+                    elementRating = docRating.getElementsByClass("imdbRating").before("pro-logo-main-tittle").first();
+
+
+                    if (elementRating == null) {
+                        ratingString = "N/A";
+                    } else {
+                        ratingString = elementRating.text();
+                        ratingString = ratingString.split(" ")[0];
+                    }
+                    sb.append(ratingString).append(" ----> ").append(entry.getValue()).append("\n");
+                    progress = progress + 1;
+                    updateProgress(progress, listSize);
+                }
+                str = sb.toString();
+                ratingTextArea.setText(str);
+                updateProgress(0, listSize);
+                searchButton.setDisable(false);
+                return null;
+            }
+        };
         Thread t1 = new Thread(task);
         fs.setSearchString(searchField.getText());
         fs.getResult();
         pb.progressProperty().bind(task.progressProperty());
         t1.start();
+
+
+
     }
 
 
